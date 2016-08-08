110ab64100cf341b14683eeca516b59531c0d9af
braully <braully@gmail.com>
2016-08-08 16:12:21 -0300

    templating

--- src/main/java/com/github/braully/bsfinance/Application.java
+++ src/main/java/com/github/braully/bsfinance/Application.java
@@ -1,67 +1,34 @@
 package com.github.braully.bsfinance;
 
-import com.sun.faces.config.ConfigureListener;
 import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  *
  * @author strike
  */
-@SpringBootApplication //@Configuration @EnableAutoConfiguration @ComponentScan
-@EntityScan(
-        basePackages = {
-            "com.github.braully.domain"
-        }
-)
-public class Application extends SpringBootServletInitializer {
+@SpringBootApplication
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-//    @Bean
-//    public ServletRegistrationBean servletRegistrationBean() {
-//        FacesServlet servlet = new FacesServlet();
-//        return new ServletRegistrationBean(servlet, "*.xhtml");
-//    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        super.onStartup(servletContext);
-        servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
-        servletContext.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".html");
-    }
+    /* JSF */
+//    @Bean
+//    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
+//        ServletRegistrationBean registration = new ServletRegistrationBean(
+//                dispatcherServlet);
+//        registration.addUrlMappings("/whatever/*", "/whatever2/*");
+//        return registration;
+//    }
 
     @Bean
-    public FacesServlet facesServlet() {
-        return new FacesServlet();
-    }
-
-    @Bean
-    public ServletRegistrationBean facesServletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(
-                facesServlet(), "*.jsf");
-        registration.setName("FacesServlet");
-        return registration;
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<ConfigureListener>(
-                new ConfigureListener());
+    public ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new FacesServlet(), "*.jsf");
     }
 }
