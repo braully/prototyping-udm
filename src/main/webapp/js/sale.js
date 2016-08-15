8a19b96619c82170a6d6736739ed62b406131d4f
braully <braully@gmail.com>
2016-08-15 15:07:56 -0300

    dynamic form test

--- src/main/webapp/js/sale.js
+++ src/main/webapp/js/sale.js
@@ -1,11 +1,6 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 angular.module('baseApp').controller('mainController', function ($scope, EntityFactory) {
     $scope.partner = [];
+//    $scope.partner = EntityFactory.get({entity: 'partner', id: '@id'})
     $scope.partners = EntityFactory.query({entity: 'partner'});
 });
 
