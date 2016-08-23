40833a11b2ef84f2f2ee0d1bfa8c428ebd4c6e82
strike <strike@alastor>
2016-08-22 23:15:26 -0300

    correção de schedule.

--- src/main/webapp/js/teste.js
+++ src/main/webapp/js/teste.js
@@ -1,4 +1,4 @@
-angular.module('baseApp').controller('mainController', function ($scope, $controller, $filter, Entity) {
+angular.module('baseApp').controller('mainController', function ($scope, $controller, Entity) {
     angular.extend(this, $controller('mainControllerBase', {$scope: $scope}));
 //    $scope.partner = Entity.get({classe: 'partner', id: '@id'})
     $scope.objects = Entity.query({classe: 'partner'});
@@ -12,11 +12,29 @@ angular.module('baseApp').controller('mainController', function ($scope, $contro
     $scope.currentPage = 1;
     $scope.numPerPage = 5;
 
+    $scope.totalPages = Math.ceil($scope.objects.length / $scope.numPerPage);
+
     $scope.paginate = function (value) {
         var begin, end, index;
         begin = ($scope.currentPage - 1) * $scope.numPerPage;
         end = begin + $scope.numPerPage;
         index = $scope.objects.indexOf(value);
         return (begin <= index && index < end);
+    };
+
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 1) {
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage * $scope.numPerPage < $scope.objects.length) {
+            $scope.currentPage++;
+        }
     };
 });
