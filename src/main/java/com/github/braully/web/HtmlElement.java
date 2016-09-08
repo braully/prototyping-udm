4c3c8e39198e503712be7197ac119e646e766d9d
strike <strike@alastor>
2016-09-07 22:10:03 -0300

    begin statistical information

--- src/main/java/com/github/braully/web/HtmlElement.java
+++ src/main/java/com/github/braully/web/HtmlElement.java
@@ -6,7 +6,12 @@
 package com.github.braully.web;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.Map;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import org.apache.commons.lang3.text.WordUtils;
 
 /**
@@ -31,6 +36,15 @@ public class HtmlElement {
     HtmlElement(Field field) {
         type = field.getType().getSimpleName();
         property = field.getName();
+        if (field.getAnnotation(OneToOne.class) != null
+                || field.getAnnotation(ManyToOne.class) != null) {
+            type = "selectone";
+        }
+        if (field.getType().isAssignableFrom(Collection.class)
+                && (field.getAnnotation(ManyToMany.class) != null
+                || field.getAnnotation(OneToMany.class) != null)) {
+            type = "selectmany";
+        }
         label = property.replaceAll("(\\p{Ll})(\\p{Lu})", "$1 $2");
         label = WordUtils.capitalize(label);
     }
